fdbaf8979e67d8d302502c8ef439dd32e0d305d8
vikramkbgs@gmail.com <vikramkbgs@gmail.com>
2022-11-16 13:15:35 +0530

    1. fixing minute and second hand 2. documenting action.js file in detail

--- action.js
+++ action.js
@@ -1,49 +1,62 @@
+// this is used for immediate execution of javascript after loading the whole page.
 window.onload = function () {
   var seconds = 00;
-  var tens = 00;
-  var appendTens = document.getElementById("tens");
+  var minutes = 00;
   var appendSeconds = document.getElementById("seconds");
+  var appendMinutes = document.getElementById("minutes");
   var buttonStart = document.getElementById("button-start");
   var buttonStop = document.getElementById("button-stop");
   var buttonReset = document.getElementById("button-reset");
   var Interval;
 
+  // method for start button
   buttonStart.onclick = function () {
     clearInterval(Interval);
+
+    // after each second  startTimer method will execute
     Interval = setInterval(startTimer, 1000);
   };
+
+  // method for stop button
   buttonStop.onclick = function () {
     clearInterval(Interval);
   };
+
+  // method for Reset button
   buttonReset.onclick = function () {
     clearInterval(Interval);
-    tens = "00";
     seconds = "00";
-    appendTens.innerHTML = tens;
+    minutes = "00";
     appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
   };
 
   function startTimer() {
-    tens++;
+    seconds++;
 
-    if (tens <= 9) {
-      appendTens.innerHTML = "0" + tens;
-    }
-
-    if (tens > 9) {
-      appendTens.innerHTML = tens;
-    }
-
-    if (tens > 60) {
-      console.log("seconds");
-      seconds++;
+    // when second is less than or equal to 9 time that time apending html text with 0.
+    if (seconds <= 9) {
       appendSeconds.innerHTML = "0" + seconds;
-      tens = 0;
-      appendTens.innerHTML = "0" + 0;
     }
 
     if (seconds > 9) {
       appendSeconds.innerHTML = seconds;
+    }
+
+    // when second is greater than 60 that time we increament the minutes hand.
+    if (seconds > 60) {
+      minutes++;
+
+      // when minutes is less than or equal to 9 time that time apending html text with 0.
+      appendMinutes.innerHTML = "0" + minutes;
+      seconds = 0;
+
+      // starting second from 00
+      appendSeconds.innerHTML = "0" + 0;
+    }
+
+    if (minutes > 9) {
+      appendSeconds.innerHTML = minutes;
     }
   }
 };
